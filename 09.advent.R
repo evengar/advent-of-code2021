ed08ec5d5a80e534e46389242ce2b8fd2135508b
evengar <even.garvang@gmail.com>
2021-12-09 09:19:26 +0100

    day 9 part 1

---
+++ 09.advent.R
@@ -0,0 +1,51 @@
+library(stringr)
+teststr <- "2199943210
+3987894921
+9856789892
+8767896789
+9899965678"
+test <- read.table(text = teststr, sep = "")
+testmat <- apply(str_split(test$V1, "", simplify = TRUE), 2, as.numeric)
+
+row_decrease <- t(apply(testmat, 1, diff)) < 0
+col_decrease <- apply(testmat, 2, diff) < 0
+invrow_decrease <- t(apply(testmat, 1, diff)) < 0
+
+
+
+
+topo <- matrix(FALSE, nrow = nrow(testmat), ncol = ncol(testmat))
+
+testmat[1,1] < testmat[c(1,2),c(1,2)]
+result <- testmat[1:2,1:2] == min(testmat[list(c(1,2), c(2,1))])
+
+testmat[1,1] < c(testmat[1,2], testmat[2,1])
+testmat[1, 2] < c(testmat[1,1], testmat[1,3], testmat[2,2])
+
+
+topo[1:2, 1:2] <- result
+result <- testmat[1:2,2:3] == min(testmat[1:2, 2:3])
+
+a <- c(2,2)
+
+
+getAdjacent <- function(mat, row, col){
+  
+  indices <- list(c(row - 1, col), c(row + 1, col), c(row, col - 1), c(row, col + 1))
+  oob <- sapply(indices, function(x) any(x < 1) | any(x > dim(mat)))
+  indices <- indices[!oob]
+  vals <- rep(NA, length(indices))
+  for (i in seq_along(indices)){
+    vals[i] <- mat[indices[[i]][1], indices[[i]][2]]
+  }
+  return(vals)
+}
+
+topo[1:2, 2:3] <- topo[1:2, 2:3] & result
+
+
+for (i in 1:nrow(testmat)){
+  for (j in 1:ncol(testmat)){
+    
+  }
+}
